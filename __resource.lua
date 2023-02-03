1a35e0e3952283baf5165dff158ac95fc0d569fc
Guudz <Kilian1702@outlook.com>
2023-02-03 02:07:33 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,23 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'client.lua'
+}
+
+files {
+	'html/ui.html',
+	'html/style.css',
+	'html/grid.css',
+	'html/main.js',
+	'html/img/posao.png',
+	'html/img/novac.png',
+	'html/img/black.png',
+	'html/img/banka.png',
+	'html/img/unemployed2.png'
+}
+
+--FOR SUPPORT:
+
+--//esegovic#1337
